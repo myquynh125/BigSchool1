43543939ede30c3432ca862b0c8ea9ba30b9b2c4
quynh <quynhnguyentm9@gmail.com>
2021-07-06 23:09:42 +0700

    THWBigSchool

---
+++ BigSchool1/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BigSchool1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+namespace BigSchool1.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objcourse = new Course();
+            objcourse.listcategory = context.Categories.ToList();
+            return View(objcourse);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            ModelState.Remove("LectureId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.listcategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LectureId = user.Id;
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BigSchool1/Models/Course.cs
@@ -0,0 +1,31 @@
+namespace BigSchool1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LectureId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int CategoryId { get; set; }
+       
+        public virtual Category Category { get; set; }
+        public List<Category> listcategory = new List<Category>();
+
+    }
+}
